f9e622e8268432eb692446fbfd565904f1abc788
Andrés Mejía <brmejiasuarez@gmail.com>
2018-06-05 05:20:54 +0200

    Reemplazado Powerlevel9k por Spaceship oh-my-zsh theme Añadido modulo de pipenv y thema Spaceship dentro del directorio oh-my-zsh

---
+++ oh-my-zsh/custom/plugins/pipenv/pipenv.plugin.zsh
@@ -0,0 +1,37 @@
+# Add your own custom plugins in the custom/plugins directory. Plugins placed
+# here will override ones with the same name in the main plugins directory.
+#compdef pipenv
+_pipenv() {
+  eval $(env COMMANDLINE="${words[1,$CURRENT]}" _PIPENV_COMPLETE=complete-zsh  pipenv)
+}
+
+_togglePipenvShell() {
+  # deactivate shell if Pipfile doesn't exit and not in a sub dir
+  if [[ ! -a "$PWD/Pipfile" ]]; then
+      if [[ "$PIPENV_ACTIVE" == 1 ]]; then
+          if [[ "$PWD" != "$pipfile_dir"* ]]; then
+            exit
+          fi
+    fi
+  fi
+
+  # activate the shell if Pipfile exists
+  if [[ "$PIPENV_ACTIVE" != 1 ]]; then
+    if [[ -a "$PWD/Pipfile" ]]; then
+      export pipfile_dir="$PWD"
+      pipenv shell
+    fi
+  fi
+}
+
+if [[ "$(basename ${(%):-%x})" != "_pipenv" ]]; then
+  autoload -U compinit && compinit
+  compdef _pipenv pipenv
+fi
+
+export PROMPT_COMMAND=_togglePipenvShell
+if [ -n "$ZSH_VERSION" ]; then
+  function chpwd() {
+    _togglePipenvShell
+  }
+fi
