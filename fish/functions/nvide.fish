f59eae0143f25d80617b1a39c5ff519c7317a26b
Andres Mejia <brmejiasuarez@gmail.com>
2022-10-13 18:17:22 +0200

    Remove the nvim function pointing to neovide.

--- fish/functions/nvide.fish
+++ fish/functions/nvide.fish
@@ -1,6 +1,6 @@
 # Defined in /home/andres/.config/fish/functions/grep.fish @ line 1
 function nvide
-    command neovide --multigrid --frame None $argv
+    command neovide --frame None $argv
 end
 
 
--- fish/functions/nvim.fish
+++
@@ -1,6 +0,0 @@
-# Defined in /home/andres/.config/fish/functions/grep.fish @ line 1
-function nvim
-    command neovide --multigrid --frame None $argv
-end
-
-
