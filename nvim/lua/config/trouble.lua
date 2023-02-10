6f12b2fa0d1691780018cb41f138b8725dcdb349
Andrés Mejía <brmejiasuarez@gmail.com>
2023-02-10 01:17:18 +0100

    Update Trouble settings to show when the buffer is saved

--- nvim/lua/config/trouble.lua
+++ nvim/lua/config/trouble.lua
@@ -35,18 +35,18 @@ require("trouble").setup {
         next = "j" -- next item
     },
     indent_lines = true, -- add an indent guide below the fold icons
-    auto_open = false, -- automatically open the list when you have diagnostics
+    auto_open = true, -- automatically open the list when you have diagnostics
     auto_close = true, -- automatically close the list when you have no diagnostics
     auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
     auto_fold = false, -- automatically fold a file trouble list at creation
     auto_jump = { "lsp_definitions" }, -- for the given modes, automatically jump if there is only a single result
-    signs = {
-        -- icons / text used for a diagnostic
-        error = "",
-        warning = "",
-        hint = "",
-        information = "",
-        other = "﫠"
-    },
-    use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
+    -- signs = {
+    --     -- icons / text used for a diagnostic
+    --     error = "",
+    --     warning = "",
+    --     hint = "",
+    --     information = "",
+    --     other = "﫠"
+    -- },
+    use_diagnostic_signs = true -- enabling this will use the signs defined in your lsp client
 }
