12f3d8a2439cb4e3dc7ff5446ae5782ff11accf2
Andrés Mejía <brmejiasuarez@gmail.com>
2023-03-06 23:49:30 +0100

    Fix error on cmdline autocompletion source.

--- nvim/lua/config/nvim-cmp.lua
+++ nvim/lua/config/nvim-cmp.lua
@@ -47,7 +47,7 @@ cmp.setup({
         -- Add tab support
         ['<S-Tab>'] = cmp.mapping.select_prev_item(),
         ['<Tab>'] = cmp.mapping.select_next_item(),
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-b>'] = cmp.mapping.scroll_docs( -4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
@@ -64,11 +64,11 @@ cmp.setup({
             vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
             -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
             vim_item.menu = ({
-                nvim_lsp = "[LSP]",
-                luasnip = "[Snip]",
-                buffer = "[Buff]",
-                path = "[Path]",
-            })[entry.source.name]
+                    nvim_lsp = "[LSP]",
+                    luasnip = "[Snip]",
+                    buffer = "[Buff]",
+                    path = "[Path]",
+                })[entry.source.name]
             return vim_item
         end,
     },
@@ -82,14 +82,13 @@ cmp.setup({
             luasnip.lsp_expand(args.body) -- For luasnip users.
         end,
     },
-
     sources = cmp.config.sources({
-        { name = 'nvim_lsp', keyword_length = 2 },
+        { name = 'nvim_lsp',               keyword_length = 2 },
         { name = 'nvim_lsp_signature_help' },
         -- { name = 'vsnip' }, -- For vsnip users.
-        { name = 'luasnip', keyword_length = 2 }, -- For luasnip users.
-        { name = 'path', keyword_length = 2 },
-        { name = 'buffer', keyword_length = 3 },
+        { name = 'luasnip',                keyword_length = 2 }, -- For luasnip users.
+        { name = 'path',                   keyword_length = 2 },
+        { name = 'buffer',                 keyword_length = 3 },
     })
 })
 
@@ -112,10 +111,12 @@ cmp.setup.cmdline('/', {
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-        { name = 'path' }
-    }, {
-        { name = 'cmdline' }
-    })
+    sources = {
+        {
+            name = 'cmdline',
+            option = {
+                ignore_cmds = {}
+            }
+        }
+    }
 })
