8446632110728333d7d8267bbcfc9974ec127476
Andrés Mejía <brmejiasuarez@gmail.com>
2023-04-16 18:35:33 +0200

    Update nvim-tree settings

--- nvim/lua/config/nvim-tree.lua
+++ nvim/lua/config/nvim-tree.lua
@@ -2,6 +2,10 @@ if not require "lib.utils".has_module("nvim-tree") then
     return
 end
 
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 if require "lib.utils".has_module("which-key") then
     local wk = require "which-key"
     local mappings = {
@@ -15,61 +19,18 @@ end
 -- setup with all defaults
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 require 'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
-    auto_reload_on_write = true,
-    disable_netrw = false,
-    hijack_cursor = false,
-    hijack_netrw = true,
-    hijack_unnamed_buffer_when_opening = false,
-    ignore_buffer_on_setup = false,
-    open_on_setup = false,
-    open_on_setup_file = false,
-    open_on_tab = false,
     sort_by = "name",
-    update_cwd = false,
     view = {
-        width = 40,
+        width = 50,
         hide_root_folder = false,
         side = "left",
         preserve_window_proportions = false,
         number = false,
         relativenumber = false,
         signcolumn = "yes",
-        mappings = {
-            custom_only = false,
-            list = {
-                -- user mappings go here
-            },
-        },
-    },
-    renderer = {
-        indent_markers = {
-            enable = false,
-            icons = {
-                corner = "└ ",
-                edge = "│ ",
-                none = "  ",
-            },
-        },
-        icons = {
-            webdev_colors = true,
-        },
-    },
-    hijack_directories = {
-        enable = true,
-        auto_open = true,
-    },
-    update_focused_file = {
-        enable = false,
-        update_cwd = false,
-        ignore_list = {},
-    },
-    ignore_ft_on_setup = {},
-    system_open = {
-        cmd = "",
-        args = {},
     },
     diagnostics = {
-        enable = false,
+        enable = true,
         show_on_dirs = false,
         icons = {
             hint = "",
@@ -78,50 +39,4 @@ require 'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
             error = "",
         },
     },
-    filters = {
-        dotfiles = false,
-        custom = {},
-        exclude = {},
-    },
-    git = {
-        enable = true,
-        ignore = true,
-        timeout = 400,
-    },
-    actions = {
-        use_system_clipboard = true,
-        change_dir = {
-            enable = true,
-            global = false,
-            restrict_above_cwd = false,
-        },
-        open_file = {
-            quit_on_open = true,
-            resize_window = true,
-            window_picker = {
-                enable = true,
-                chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-                exclude = {
-                    filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
-                    buftype = { "nofile", "terminal", "help" },
-                },
-            },
-        },
-    },
-    trash = {
-        cmd = "trash",
-        require_confirm = true,
-    },
-    log = {
-        enable = false,
-        truncate = false,
-        types = {
-            all = false,
-            config = false,
-            copy_paste = false,
-            diagnostics = false,
-            git = false,
-            profile = false,
-        },
-    },
-} -- END_DEFAULT_OPTS
+}
