f69d101531948509c60dcd790f77ea4f801e9d93
Andrés Mejía <brmejiasuarez@gmail.com>
2023-05-24 01:55:53 +0200

    Fix floating poisiton of hlslens

--- nvim/lua/config/hlslens.lua
+++ nvim/lua/config/hlslens.lua
@@ -1,9 +1,14 @@
-if not require"lib.utils".has_module("hlslens") then
+if not require "lib.utils".has_module("hlslens") then
     return
 end
 
-require('hlslens').setup({
-    calm_down = true,
-    nearest_only = true,
-    nearest_float_when = 'always'
-})
+local hlslens = require "hlslens"
+
+vim.opt.hlsearch = true
+
+hlslens.setup(
+    {
+        calm_down = true,
+        nearest_only = true,
+    }
+)
