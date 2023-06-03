640f6bba172c69e37fbc8381b5363712814404c0
Andrés Mejía <brmejiasuarez@gmail.com>
2023-06-03 11:41:48 +0200

    feat(nvim): Add quick-scope plugin

--- nvim/lua/mesuca/legacy_packer_plugins/quick-scope.lua
+++
@@ -1,8 +0,0 @@
--- if not require"lib.utils".has_module("quickscope") then
---     return
--- end
-
-vim.cmd([[
-" Trigger a highlight in the appropriate direction when pressing these keys:
-let g:qs_highlight_on_keys = ['f', 'F', 't', 'T']
-]])
