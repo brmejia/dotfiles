1e6a5163ebcb6dd30c54c2989814309d12f7a372
Andrés Mejía <brmejiasuarez@gmail.com>
2023-06-14 02:52:23 +0200

    feat(nvim): Update settings for Mason ensuring installed packages

--- nvim/lua/mesuca/plugins/lsp/mason-lspconfig.lua
+++ nvim/lua/mesuca/plugins/lsp/mason-lspconfig.lua
@@ -3,16 +3,39 @@ return {
     dependencies = { "williamboman/mason.nvim" },
     opts = {
         ensure_installed = {
-            "pylsp",
-            "ruff_lsp",
-            "lua_ls",
+            "python-lsp-server",
+            "ruff-lsp",
+            "lua-language-server",
+            -- "mypy",
+            -- "ruff",
+            -- "black",
             -- "rust_analyzer",
             "taplo", -- TOML Files
-            "volar",
-            "tsserver",
-            "denols",
-            "tailwindcss",
-            "eslint",
+            "vue-language-server",
+            "typescript-language-server",
+            "deno",
+            "tailwindcss-language-server",
+            "eslint-lsp",
+            "prettierd",
+            "stylua",
         },
     },
+    ---@param opts MasonSettings | {ensure_installed: string[]}
+    config = function(_, opts)
+        require("mason").setup(opts)
+        local mr = require("mason-registry")
+        local function ensure_installed()
+            for _, tool in ipairs(opts.ensure_installed) do
+                local p = mr.get_package(tool)
+                if not p:is_installed() then
+                    p:install()
+                end
+            end
+        end
+        if mr.refresh then
+            mr.refresh(ensure_installed)
+        else
+            ensure_installed()
+        end
+    end,
 }
