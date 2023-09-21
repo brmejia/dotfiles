0d9f6ed87a2fb13d74ce8a9fa3b2f493d6addd54
Andrés Mejía <brmejiasuarez@gmail.com>
2023-09-21 20:07:26 +0200

    feat(nvim): Add tabnine config file

--- nvim/lua/mesuca/plugins/tabnine.lua
+++ nvim/lua/mesuca/plugins/tabnine.lua
@@ -2,12 +2,15 @@ return {
     "codota/tabnine-nvim",
     enabled = false,
     build = "./dl_binaries.sh",
-    -- opts = {
-    --     -- disable_auto_comment = true,
-    --     -- accept_keymap = "<Tab>",
-    --     -- dismiss_keymap = "<C-]>",
-    --     -- debounce_ms = 800,
-    --     -- suggestion_color = { gui = "#808080", cterm = 244 },
-    --     -- exclude_filetypes = { "TelescopePrompt" }
-    -- }
+    opts = {
+        disable_auto_comment = true,
+        accept_keymap = "<Right>",
+        dismiss_keymap = "<C-]>",
+        debounce_ms = 800,
+        -- suggestion_color = { gui = "#808080", cterm = 244 },
+        exclude_filetypes = { "TelescopePrompt", "neo-tree", "harpoon" },
+    },
+    config = function(_, opts)
+        require("tabnine").setup(opts)
+    end,
 }
