3d19293997c32925fec3fdcea7f3b81428b7614d
Andrés Mejía <brmejiasuarez@gmail.com>
2023-10-11 20:53:43 +0200

    fix(nvim): Fix crates completion for cargo.toml files

--- nvim/lua/mesuca/plugins/languages/rust/crates.lua
+++ nvim/lua/mesuca/plugins/languages/rust/crates.lua
@@ -1,26 +1,45 @@
 return {
     "saecki/crates.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
+    config = function(_, opts)
         local crates = require("crates")
-        crates.setup({
-            null_ls = {
-                enabled = true,
-                name = "crates",
-            },
-        })
 
         if require("lib.utils").has_module("null-ls") then
+            opts = vim.tbl_extend("keep", opts, {
+                null_ls = {
+                    enabled = true,
+                    name = "crates",
+                },
+            })
+        else
+            vim.notify("Crates.nvim code actions require the package null-ls", vim.log.levels.WARN)
+        end
+
+        if require("lib.utils").has_module("cmp") then
             local cmp = require("cmp")
 
             vim.api.nvim_create_autocmd("BufRead", {
                 group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
                 pattern = "Cargo.toml",
                 callback = function()
-                    cmp.setup.buffer({ sources = { { name = "crates" } } })
+                    local srcs = vim.tbl_deep_extend(
+                        "keep",
+                        cmp.config.sources(),
+                        { sources = { { name = "crates", keyword_length = 0 } } }
+                    )
+                    cmp.setup.buffer(srcs)
                 end,
             })
+
+            opts = vim.tbl_extend("keep", opts, {
+                src = {
+                    cmp = {
+                        enabled = true,
+                    },
+                },
+            })
         end
+        crates.setup(opts)
     end,
     -- event = { "BufRead Cargo.toml" },
     ft = { "rust", "toml" },
