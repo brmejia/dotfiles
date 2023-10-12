846799ed162b90cc68ac51aff4d20dc4ad7db494
Andrés Mejía <brmejiasuarez@gmail.com>
2023-10-12 22:14:16 +0200

    fix(nvim): Fix volar root dir function in order to work with Nuxt modules by providing a prioritized list of filenames to lookup

--- nvim/lua/mesuca/plugins/lsp/server_configs/volar.lua
+++ nvim/lua/mesuca/plugins/lsp/server_configs/volar.lua
@@ -1,18 +1,12 @@
 -- local lspconfig = require("lspconfig")
 -- local lspconfig_configs = require("lspconfig.configs")
+local lspconfig_util = require("lspconfig.util")
 
 local function on_new_config(new_config, new_root_dir)
     local function get_typescript_server_path(root_dir)
         local project_root = lspconfig_util.find_node_modules_ancestor(root_dir)
         return project_root
-                and (lspconfig_util.path.join(
-                    project_root,
-                    "package.json",
-                    "node_modules",
-                    "typescript",
-                    "lib",
-                    "tsserverlibrary.js"
-                ))
+                and (lspconfig_util.path.join(project_root, "node_modules", "typescript", "lib", "tsserverlibrary.js"))
             or ""
     end
 
@@ -25,18 +19,58 @@ local function on_new_config(new_config, new_root_dir)
     end
 end
 
-local function root_dir(...)
-    local lspconfig_util = require("lspconfig.util")
-    local rdir = lspconfig_util.root_pattern("package.json")(...)
+local function get_volar_root_dir(file_path)
+    local options = {
+        { ".nuxtrc", ".npmrc" },
+        { "package.json", "yarn.lock" },
+    }
+    local rdir = nil
+    print("get_volar_root_dir(file_path)", vim.inspect(file_path))
+    for _, value in pairs(options) do
+        -- print("table.unpack(value)", table.unpack(value))
+        rdir = lspconfig_util.root_pattern(table.unpack(value))(file_path)
+        if rdir ~= "" then
+            local msg = "Found volar root dir " .. rdir .. " with " .. vim.inspect(value)
+            print(msg)
+            vim.notify(msg, vim.log.levels.DEBUG)
+            break
+        end
+        local msg = "Not found volar for " .. vim.inspect(value)
+        print(msg)
+        vim.notify(msg, vim.log.levels.DEBUG)
+    end
     return rdir
 end
 
 local config = {
     -- cmd = { "vue-language-server", "--stdio" }, -- commented in order to use the binary installed with mason
-
-    -- root_dir = root_dir,
+    root_dir = get_volar_root_dir,
     -- on_new_config = on_new_config,
     filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+    init_options = {
+        typescript = {
+            tsdk = "",
+        },
+        languageFeatures = {
+            implementation = true, -- new in @volar/vue-language-server v0.33
+            references = true,
+            definition = true,
+            typeDefinition = true,
+            callHierarchy = true,
+            hover = true,
+            rename = true,
+            renameFileRefactoring = true,
+            signatureHelp = true,
+            codeAction = true,
+            workspaceSymbol = true,
+            completion = {
+                defaultTagNameCase = "both",
+                defaultAttrNameCase = "kebabCase",
+                getDocumentNameCasesRequest = false,
+                getDocumentSelectionRequest = false,
+            },
+        },
+    },
 }
 
 return config
