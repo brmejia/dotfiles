9a6ffb22c463a3ffa0878ef3bc3cfc79ba030f60
Andrés Mejía <brmejiasuarez@gmail.com>
2023-10-13 01:31:15 +0200

    refactor(nvim): Remove from Telescope config file the dependencie of which-key to define the keymaps feat(nvim): Integrate Telescope pickers selection with Trouble

--- nvim/lua/mesuca/plugins/ui/telescope.lua
+++ nvim/lua/mesuca/plugins/ui/telescope.lua
@@ -3,67 +3,83 @@ return {
         "nvim-telescope/telescope.nvim",
         branch = "0.1.x",
         dependencies = { { "nvim-lua/plenary.nvim" } },
-        config = function()
-            if not require("lib.utils").has_module("telescope") then
-                return
-            end
+        opts = function()
+            local actions = require("telescope.actions")
 
-            local telescope = require("telescope")
-
-            telescope.setup({
+            local opts = {
+                defaults = {
+                    layout_strategy = "horizontal",
+                    layout_config = {
+                        vertical = { width = 0.95, height = 0.95 },
+                        horizontal = { width = 0.95, height = 0.95 },
+                    },
+                    mappings = {
+                        i = {
+                            ["<C-Down>"] = "cycle_history_next",
+                            ["<C-Up>"] = "cycle_history_prev",
+                            ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+                        },
+                        n = {
+                            ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+                        },
+                    },
+                    history = {
+                        path = "~/.local/share/nvim/databases/telescope_history.sqlite3",
+                        limit = 100,
+                    },
+                },
+                -- other configuration values here
                 extensions = {
                     ["ui-select"] = {
                         require("telescope.themes").get_dropdown({
                             -- even more opts
                         }),
-
-                        -- pseudo code / specification for writing custom displays, like the one
-                        -- for "codeactions"
-                        -- specific_opts = {
-                        --   [kind] = {
-                        --     make_indexed = function(items) -> indexed_items, width,
-                        --     make_displayer = function(widths) -> displayer
-                        --     make_display = function(displayer) -> function(e)
-                        --     make_ordinal = function(e) -> string
-                        --   },
-                        --   -- for example to disable the custom builtin "codeactions" display
-                        --      do the following
-                        --   codeactions = false,
-                        -- }
                     },
                 },
-            })
-            -- To get ui-select loaded and working with telescope, you need to call
-            -- load_extension, somewhere after setup function:
-            require("telescope").load_extension("ui-select")
+            }
 
-            if require("lib.utils").has_module("which-key") then
-                local wk = require("which-key")
-                local tbuiltin = require("telescope.builtin")
-
-                local leader_mappings = {
-                    f = {
-                        name = "Search",
-                        b = { tbuiltin.buffers, "Buffers" },
-                        f = { tbuiltin.live_grep, "RipGrep" },
-                        p = { tbuiltin.find_files, "Files" },
-                        m = { tbuiltin.keymaps, "Keymaps" },
-                        h = { tbuiltin.help_tags, "Help Tags" },
+            if require("lib.utils").has_module("trouble") then
+                local trouble = require("trouble.providers.telescope")
+                opts = vim.tbl_deep_extend("force", opts, {
+                    defaults = {
+                        mappings = {
+                            i = { ["<C-q>"] = trouble.smart_open_with_trouble },
+                            n = { ["<C-q>"] = trouble.smart_open_with_trouble },
+                        },
                     },
-                }
-
-                local opts = { prefix = "<leader>" }
-                wk.register(leader_mappings, opts)
-
-                local opts = { mode = "n", noremap = true }
-                wk.register({
-                    ["<C-p>"] = { tbuiltin.find_files, "Files" },
-                }, opts)
+                })
             end
+
+            return opts
+        end,
+        config = function(_, opts)
+            local telescope = require("telescope")
+            telescope.setup(opts)
+
+            local keymap = require("lib.utils").keymap
+            local tbuiltin = require("telescope.builtin")
+
+            keymap("n", "<C-p>", tbuiltin.find_files, { desc = "Find files" })
+            keymap("n", "<leader>fp", tbuiltin.find_files, { desc = "Find files" })
+            keymap("n", "<leader>ff", tbuiltin.live_grep, { desc = "RipGrep" })
+            keymap("n", "<leader>fh", tbuiltin.help_tags, { desc = "Help Tags" })
+            keymap("n", "<leader>fm", tbuiltin.keymaps, { desc = "Keymaps" })
         end,
     },
     {
         "nvim-telescope/telescope-ui-select.nvim",
-        dependencies = { { "nvim-telescope/telescope.nvim" } },
+        dependencies = { "nvim-telescope/telescope.nvim" },
+        config = function(_, opts)
+            -- To get ui-select loaded and working with telescope, you need to call
+            -- load_extension, somewhere after setup function:
+            require("telescope").load_extension("ui-select")
+        end,
+    },
+    {
+        "nvim-telescope/telescope-smart-history.nvim",
+        dependencies = { "kkharji/sqlite.lua", "nvim-telescope/telescope.nvim" },
+        config = function(_, opts)
+            require("telescope").load_extension("smart_history")
+        end,
     },
 }
