2b92338bc12dd153f5dea26e52fa5911af64506b
Andrés Mejía <brmejiasuarez@gmail.com>
2023-11-10 22:17:18 +0100

    feat(nvim): Add keymap for refactoring.nvim

--- nvim/lua/mesuca/plugins/refactoring.lua
+++ nvim/lua/mesuca/plugins/refactoring.lua
@@ -3,8 +3,17 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
+        "nvim-telescope/telescope.nvim",
     },
-    -- config = function()
-    --     require("refactoring").setup()
-    -- end,
+    config = function()
+        local keymap = require("lib.utils").keymap
+        local telescope = require("telescope")
+
+        -- load refactoring Telescope extension
+        telescope.load_extension("refactoring")
+
+        keymap({ "n", "x" }, "<leader>rr", telescope.extensions.refactoring.refactors, { desc = "Refactoring" })
+
+        require("refactoring").setup()
+    end,
 }
