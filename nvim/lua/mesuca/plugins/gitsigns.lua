3c0cf919165d593ddbe8d825274e661a5f9080b1
Andrés Mejía <candres.mejias@proton.me>
2024-06-25 08:27:33 +0200

    fix(gitsigns): Fix breaking change on signs configuration

--- nvim/lua/mesuca/plugins/gitsigns.lua
+++ nvim/lua/mesuca/plugins/gitsigns.lua
@@ -5,34 +5,12 @@ return {
 
         gs.setup({
             signs = {
-                add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-                change = {
-                    hl = "GitSignsChange",
-                    text = "▎",
-                    numhl = "GitSignsChangeNr",
-                    linehl = "GitSignsChangeLn",
-                },
-                delete = {
-                    hl = "GitSignsDelete",
-                    text = "󰦺",
-                    -- text = "_",
-                    numhl = "GitSignsDeleteNr",
-                    linehl = "GitSignsDeleteLn",
-                },
-                topdelete = {
-                    hl = "GitSignsDelete",
-                    text = "󰧆",
-                    -- text = "‾",
-                    numhl = "GitSignsDeleteNr",
-                    linehl = "GitSignsDeleteLn",
-                },
-                changedelete = {
-                    hl = "GitSignsChange",
-                    text = "▎",
-                    numhl = "GitSignsChangeNr",
-                    linehl = "GitSignsChangeLn",
-                },
-                untracked = { hl = "GitSignsAdd", text = "┆", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+                add = { text = "▎" },
+                change = { text = "▎" },
+                delete = { text = "󰦺" },
+                topdelete = { text = "󰧆" },
+                changedelete = { text = "▎" },
+                untracked = { text = "┆" },
             },
         })
 
