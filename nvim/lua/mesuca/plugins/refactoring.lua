cfc617c0511cf387b82d09397247608d9517e5c4
Andrés Mejía <candres.mejias@proton.me>
2024-09-18 22:45:23 +0200

    feat(nvim): Remove refactoring and telescope coupling. Using Built-In Neovim Selection instead of Telescope

--- nvim/lua/mesuca/plugins/refactoring.lua
+++ nvim/lua/mesuca/plugins/refactoring.lua
@@ -3,16 +3,11 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
-        "nvim-telescope/telescope.nvim",
     },
     config = function()
         local keymap = require("lib.utils").keymap
-        local telescope = require("telescope")
 
-        -- load refactoring Telescope extension
-        telescope.load_extension("refactoring")
-
-        keymap({ "n", "x" }, "<leader>R", telescope.extensions.refactoring.refactors, { desc = "Refactoring" })
+        keymap({ "n", "x" }, "<leader>R", require("refactoring").select_refactor, { desc = "Refactoring" })
 
         require("refactoring").setup()
     end,
