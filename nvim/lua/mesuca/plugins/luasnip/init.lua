80e2a83429a2caf791fb2a0c8dcc3aa91abbe495
Andrés Mejía <candres.mejias@proton.me>
2024-11-17 14:32:33 +0100

    feat(nvim): Upgrade luasnip version to v2.*

--- nvim/lua/mesuca/plugins/luasnip/init.lua
+++ nvim/lua/mesuca/plugins/luasnip/init.lua
@@ -1,7 +1,8 @@
 return {
     "L3MON4D3/LuaSnip",
+    enabled = true,
     -- follow latest release.
-    version = "1.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
     -- install jsregexp (optional!).
     build = "make install_jsregexp",
     dependencies = { "rafamadriz/friendly-snippets" },
