e80da32045a3f320fa563d60a3236f4f91714a41
Andrés Mejía <candres.mejias@proton.me>
2025-05-16 08:10:00 +0200

    refactor(nvim): Move lsp configuration away from nvim-lspconfig to lsp dedicated file

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,18 +1,23 @@
+local lsp = require("lib.lsp")
+
 vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(ev)
         local client = vim.lsp.get_client_by_id(ev.data.client_id)
 
-        local lsp = require("lib.lsp")
         lsp.on_attach(client, ev.buf)
     end,
 })
 
+lsp.setup_diagnostics()
+
 vim.lsp.enable({
     "lua_ls",
     -- "rust-analyzer", --- Disabled because rustaceanvim activates it
+    -- "vue-language-server",
     "volar",
     "ruff",
-    "pyright",
+    "ty",
+    -- "pyright",
     "tailwindcss",
     "ltex",
     "tinymist",
